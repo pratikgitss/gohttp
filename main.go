7d6e2f9183d61183b45c9a8cc1a1169e5b4c2b5d
Pratik Wasekar <pratikwasekar13@gmail.com>
2025-12-08 19:46:49 +0530

    serving files

--- main.go
+++ main.go
@@ -7,12 +7,18 @@ import (
 
 func main() {
 	const port = "8080"
+	const filepath = "."
 	mux := http.NewServeMux()
 
 	server := &http.Server{
 		Handler: mux,
 		Addr:    ":" + port,
 	}
+
+	mux.Handle("/", http.FileServer(http.Dir(filepath)))
+	mux.Handle("/assets/logo.png", http.FileServer(http.Dir(filepath)))
+
 	log.Printf("Serving on port: %s\n", port)
 	log.Fatal(server.ListenAndServe())
+
 }
