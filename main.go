d7f003fe8baee0d9c0d1a3ac9ae7cbe10b08ebb6
Pratik Wasekar <pratikwasekar13@gmail.com>
2025-12-09 18:50:49 +0530

    Middlewares and statefulReqs

--- main.go
+++ main.go
@@ -1,14 +1,36 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"sync/atomic"
 )
+
+type apiConfig struct {
+	fileserverHits atomic.Int32
+}
 
 func readinessHandler(w http.ResponseWriter, _ *http.Request) {
 	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
 	w.WriteHeader(http.StatusOK)
 	w.Write([]byte("OK"))
+}
+
+func (cfg *apiConfig) handlerReqCounts(w http.ResponseWriter, _ *http.Request) {
+	w.Write([]byte(fmt.Sprintf("Hits: %d", cfg.fileserverHits.Load())))
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
+		cfg.fileserverHits.Add(1)
+		next.ServeHTTP(w, req)
+	})
+}
+
+func (cfg *apiConfig) handlerResetCount(w http.ResponseWriter, req *http.Request) {
+	cfg.fileserverHits.Store(0)
+	w.WriteHeader(http.StatusOK)
 }
 
 func main() {
@@ -20,13 +42,15 @@ func main() {
 		Handler: mux,
 		Addr:    ":" + port,
 	}
+	cfg := apiConfig{}
 
 	fs := http.FileServer(http.Dir(filepath))
 
-	mux.Handle("/app/", http.StripPrefix("/app/", fs))
-
+	mux.Handle("/app/", cfg.middlewareMetricsInc(http.StripPrefix("/app/", fs)))
 	mux.HandleFunc("/healthz", readinessHandler)
 
+	mux.HandleFunc("/metrics", cfg.handlerReqCounts)
+	mux.HandleFunc("/reset", cfg.handlerResetCount)
 	log.Printf("Serving on port: %s\n", port)
 	log.Fatal(server.ListenAndServe())
 
